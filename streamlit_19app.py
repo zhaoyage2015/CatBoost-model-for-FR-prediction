40abba1f2b2a5420055406592b24d1f4e12c4aad
zhaoyage2015 <zhaoyage2015@outlook.com>
2024-12-20 16:57:47 +0800

    Update streamlit_19app.py

--- streamlit_19app.py
+++ streamlit_19app.py
@@ -22,7 +22,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 # 定义特征名称
 feature_names = ['NLR', 'Pulmonary infection', 'ASPECTS', 'Serum glucose', 'Hypertension',
-                 'Hemorrhagic transformation_1', 'Initial NIHSS', 'Baseline DBP',
+                 'Symptomatic HT', 'Initial NIHSS', 'Baseline DBP',
                  'Neutrophils', 'Age', 'MLS', 'AGR']
 
 # 输入控件
@@ -31,7 +31,7 @@ PI = st.selectbox("Pulmonary infection (0=NO, 1=Yes):", options=[0, 1], format_f
 ASPECTS = st.number_input("ASPECTS:", min_value=5, max_value=10, value=8)
 HTN = st.selectbox("Hypertension (0=NO, 1=Yes):", options=[0, 1], format_func=lambda x: 'NO (0)' if x == 0 else 'Yes (1)')
 GLU = st.number_input("Serum glucose:", min_value=2.2, max_value=32.0, value=8.0)
-SHT = st.selectbox("Hemorrhagic transformation_1 (0=NO, 1=Yes):", options=[0, 1], format_func=lambda x: 'NO (0)' if x == 0 else 'Yes (1)')
+SHT = st.selectbox("Symptomatic HT (0=NO, 1=Yes):", options=[0, 1], format_func=lambda x: 'NO (0)' if x == 0 else 'Yes (1)')
 NIHSS = st.number_input("Initial NIHSS:", min_value=3, max_value=42, value=16)
 MLS = st.number_input("MLS:", min_value=0.00, max_value=30.00, value=2.88)
 DBP = st.number_input("Baseline DBP:", min_value=40, max_value=160, value=85)
